4bdd6e072faec536ea2c9b1fbf1265faac08942c
mlmaxi98 <cardozojoaquinm@gmail.com>
2021-11-25 20:59:43 -0300

    solucionando paginado

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,12 +4,14 @@ import { HomeDiv } from './StyledHome'
 import Loading from '../Loading/Loading'
 import Card from './Card/Card'
 import { getCountries } from '../../redux/reducer'
+
 export const Home = () => {
 
     const dispatch = useDispatch();
+
+    //store
     const countries = useSelector(state => state.countries)
     const next = useSelector(state => state.next)
-    const prev = useSelector(state => state.prev)
     const loading = useSelector(state => state.loading)
     const pages = useSelector(state => state.pages)
 
@@ -29,39 +31,39 @@ export const Home = () => {
     const handleForm = (event) => {
         setForm({
             ...form,
+            page: 0,
             [event.target.name]: event.target.value
         })
     }
 
-    const nextPage = () => {
-        if (next) setForm({
-            ...form,
-            page: form.page + 1,
-        })
-        else setForm({
-            ...form,
-            page: 0,
-        })
-    }
+    const nextPage = () => setForm(next
+        ? { ...form, page: form.page + 1 }
+        : { ...form, page: 0 })
 
-    const prevPage = () => {
-        if (prev) setForm({
-            ...form,
-            page: form.page - 1,
-        })
-        else setForm({
-            ...form,
-            page: pages,
-        })
 
-    }
-
-    const handleReset = () => setForm({ ...initialForm })
+    const prevPage = () => setForm(form.page !== 0
+        ? { ...form, page: form.page - 1, }
+        : { ...form, page: pages, })
 
     useEffect(() => {
         dispatch(getCountries(form))
     }, [dispatch, form])
 
+    const regions = [
+        { value: '', name: '' },
+        { value: 'Africa', name: 'África' },
+        { value: 'Asia', name: 'Asia' },
+        { value: 'Americas', name: 'América' },
+        { value: 'Europe', name: 'Europa' },
+        { value: 'Oceania', name: 'Oceanía' },
+        { value: 'Polar', name: 'Polar' },
+    ]
+    const seasons = ['', 'Verano', 'Otoño', 'Invierno', 'Primavera']
+
+    const ordering = [
+        { value: 'ASC', name: 'Ascendente' },
+        { value: 'DESC', name: 'Descendente' },
+    ]
 
     return (
         <HomeDiv>
@@ -77,96 +79,109 @@ export const Home = () => {
                                 className='search'
                                 placeholder='Buscar'
                                 name='name'
-                                onChange={handleForm} />
+                                onChange={handleForm}
+                                autoComplete="off"
+                            />
                             <button className='buscar' onClick={handleForm}>
                                 <i className="fas fa-search" />
                             </button>
 
                         </div>
-                        {
-                            countries.length > 0
-                            && <div className='filter'>
-                                <div className='ContAct'>
 
-                                    <div className='continente'>
-                                        <div className='filCont'>
-                                            <label>Continente:</label>
-                                            <select name='region' onChange={handleForm}>
-                                                <option value=''></option>
-                                                <option value='Africa'>Africa</option>
-                                                <option value='Asia'>Asia</option>
-                                                <option value='Americas'>Americas</option>
-                                                <option value='Europe'>Europe</option>
-                                                <option value='Oceania'>Oceania</option>
-                                                <option value='Polar'>Polar</option>
-                                            </select>
-                                        </div>
+                        <div className='filter'>
+                            <div className='ContAct'>
 
-                                        <div className='order'>
-                                            <label>Ordenar por: </label>
-                                            <select name='order' onChange={handleForm}>
-                                                <option value='ASC'>ASC</option>
-                                                <option value='DESC'>DESC</option>
-                                            </select>
-                                        </div>
+                                <div className='continente'>
+                                    <div className='filCont'>
+                                        <label>Continente:</label>
+                                        <select name='region' onChange={handleForm}>
+                                            {
+                                                regions.map(region =>
+                                                    <option key={region.name}
+                                                        value={region.value}
+                                                        label={region.name} />
+                                                )
+                                            }
+                                        </select>
                                     </div>
 
-                                    <div className='Actividades'>
-
-                                        <div className='temp'>
-                                            <label>Temporada:</label>
-                                            <select name='season' onChange={handleForm}>
-                                                <option value=''></option>
-                                                <option value='Verano'>Verano</option>
-                                                <option value='Invierno'>Invierno</option>
-                                                <option value='Otoño'>Otoño</option>
-                                                <option value='Primavera'>Primavera</option>
-                                            </select>
-                                        </div>
-
-                                        <div className='actFilters'>
-
-                                            <div className='Duracion'>
-                                                <label>Duración</label>
-                                                <select name='duration' onChange={handleForm}>
-                                                    <option value=''></option>
-                                                    <option value='1' >1</option>
-                                                    <option value='2'>2</option>
-                                                    <option value='3'>3</option>
-                                                    <option value='4'>4</option>
-                                                    <option value='5'>5 o + </option>
-                                                </select>
-                                            </div>
-
-                                            <div className='Dificultad'>
-                                                <label>Dificultad</label>
-                                                <select name='difficult' onChange={handleForm}>
-                                                    <option value=''></option>
-                                                    <option value='1'> 1 </option>
-                                                    <option value='2'> 2 </option>
-                                                    <option value='3'> 3 </option>
-                                                    <option value='4'> 4 </option>
-                                                    <option value='5'> 5 </option>
-                                                </select>
-                                            </div>
-
-                                        </div>
+                                    <div className='order'>
+                                        <label>Ordenar por: </label>
+                                        <select name='order' onChange={handleForm}>
+                                            {
+                                                ordering.map(order =>
+                                                    <option key={order.name}
+                                                        value={order.value}
+                                                        label={order.name} />
+                                                )
+                                            }
+                                        </select>
                                     </div>
                                 </div>
 
-                                <div className='paginado' >
-                                    <div className='pagination-bot'>
-                                        <button className='np' onClick={prevPage}>
-                                            <i className="fas fa-arrow-left" />
-                                        </button>
-                                        <input className='count' value={form.page + 1} disabled />
-                                        <button className='np' onClick={nextPage}>
-                                            <i className="fas fa-arrow-right" />
-                                        </button>
+                                <div className='Actividades'>
+
+                                    <div className='temp'>
+                                        <label>Temporada:</label>
+                                        <select name='season' onChange={handleForm}>
+                                            {
+                                                seasons.map(season =>
+                                                    <option
+                                                        key={season}
+                                                        value={season}
+                                                        label={season} />
+                                                )
+                                            }
+                                        </select>
+                                    </div>
+
+                                    <div className='actFilters'>
+
+                                        <div className='Duracion'>
+                                            <label>Duración</label>
+                                            <select name='duration' onChange={handleForm}>
+                                                {
+                                                    Array(6).fill(0).map((n, i) =>
+                                                        <option key={i}
+                                                            value={i === 0 ? '' : (n + i).toString()}>
+                                                            {i === 0 ? ''
+                                                                : i === 5 ? '5 o +' : (n + i)}
+                                                        </option>
+                                                    )
+                                                }
+                                            </select>
+                                        </div>
+
+                                        <div className='Dificultad'>
+                                            <label>Dificultad</label>
+                                            <select name='difficult' onChange={handleForm}>
+                                                {
+                                                    Array(6).fill(0).map((n, i) =>
+                                                        <option key={i + 6}
+                                                            value={i === 0 ? '' : (n + i).toString()}>
+                                                            {i === 0 ? '' : (n + i)}
+                                                        </option>
+                                                    )
+                                                }
+                                            </select>
+                                        </div>
+
                                     </div>
                                 </div>
                             </div>
-                        }
+
+                            <div className='paginado' >
+                                <div className='pagination-bot'>
+                                    <button className='np' onClick={prevPage}>
+                                        <i className="fas fa-arrow-left" />
+                                    </button>
+                                    <input className='count' value={form.page + 1} disabled />
+                                    <button className='np' onClick={nextPage}>
+                                        <i className="fas fa-arrow-right" />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -175,9 +190,10 @@ export const Home = () => {
                         loading
                             ? <Loading />
                             : countries.length > 0
-                                ? countries.map(country => <Card
-                                    country={country}
-                                    key={country.id} />)
+                                ? countries.map(country =>
+                                    <Card
+                                        key={country.id}
+                                        country={country} />)
                                 : <span>No se encontraron resultados</span>
                     }
                 </div>
