3ab033eb1fb0ba8591978aa4d5fd80d49220ce66
mlmaxi98 <cardozojoaquinm@gmail.com>
2021-11-27 17:31:27 -0300

    arreglando card de activity

--- client/src/components/Activities/Activities.jsx
+++ client/src/components/Activities/Activities.jsx
@@ -7,13 +7,13 @@ const Activities = ({ activities }) => {
             <div className='titulo'><span>Actividades</span></div>
 
             <div className='actividades'>
-                {activities && activities.length > 0 ? activities.map((actividad) => {
-                    return <div className='notas' key={actividad.id}>
-                        <div className='nombre'>{actividad.Nombre}</div>
+                {activities && activities.length > 0 ? activities.map((activity) => {
+                    return <div className='notas' key={activity.id}>
+                        <div className='nombre'>{activity.name}</div>
                         <div className='detalles'>
-                            <div><span>Dificultad: {actividad.Dificultad}</span></div>
-                            <div><span>Duracion: {actividad.Duracion}</span></div>
-                            <div><span>Temporada: {actividad.Temporada}</span></div>
+                            <div><span>Dificultad: {activity.difficult}</span></div>
+                            <div><span>Duracion: {activity.duration}</span></div>
+                            <div><span>Temporada: {activity.season}</span></div>
                         </div>
                     </div>
                 }) : <div className='notFound'><h1>No se registraron Actividades en este país</h1></div>
